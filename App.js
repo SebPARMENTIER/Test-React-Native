7c554d26e7223f2162ce3c42a34fd44724b3ace8
SebPARMENTIER <sebastien.parmentier@outlook.fr>
2022-02-17 17:27:02 +0100

    First test

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
+  const [randomImage, setRandomImage] = useState('https://random-d.uk/api/108.jpg');
+
+  const getRandomImage = async () => {
+    const res = await fetch('https://random-d.uk/api/v2/random');
+    const data = await res.json();
+    setRandomImage(data.url);
+  };
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{paddingBottom: 20}}>Cliquez sur le bouton pour faire apparaître l'image...</Text>
+      
+        <Image
+          source={{uri: randomImage}}
+          style={{width: 200, height: 200, marginBottom: 20}}
+        />
+      
+      <Button onPress={getRandomImage} title="Click me"/>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +31,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'green',
     alignItems: 'center',
     justifyContent: 'center',
   },
